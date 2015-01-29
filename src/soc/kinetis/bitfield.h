60058cd2aa9874fa4a644c953f3db1b457f8de57
Simon Schubert <2@0x2c.org>
2015-01-29 01:59:32 +0100

    bitfield: push single-bit constants

--- src/soc/kinetis/bitfield.h
+++ src/soc/kinetis/bitfield.h
@@ -8,9 +8,20 @@
 static inline uint32_t
 bf_set1(uint32_t locval, int bitpos, uint32_t bitmask, uint32_t val)
 {
+        if (__builtin_constant_p(bitmask) &&
+            __builtin_constant_p(bitpos) &&
+            bitmask == (1 << bitpos) &&
+            __builtin_constant_p(val)) {
+                /* single bit op */
+                if (val) {
+                        return (locval | bitmask);
+                } else {
+                        return (locval & ~bitmask);
+                }
+        }
         return ((locval & ~bitmask) | (val << bitpos));
 }
 
 
-#define bf_get(loc, name) \
+#define bf_get(loc, name)                               \
         (((loc) & (name ## _MASK)) >> (name ## _SHIFT))
