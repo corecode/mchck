947d1d9393c8c73bd084d62d371345ff3057ab24
Simon Schubert <2@0x2c.org>
2015-01-31 18:40:56 +0100

    real subdir fix

--- build/mk/subdir.mk
+++ build/mk/subdir.mk
@@ -1,6 +1,12 @@
-all: ${SUBDIRS}
+.DEFAULT: subdirs
 
-${SUBDIRS}:
-	@${MAKE} -C "$@" ${MAKECMDGOALS}
+subdirs: ${SUBDIRS}
+
+define mksubdir
+$1:
+	@$${MAKE} -C "$$@" $${MAKECMDGOALS}
+endef
+
+$(foreach d,${SUBDIRS},$(eval $(call mksubdir,$d)))
 
 .PHONY: ${SUBDIRS}
