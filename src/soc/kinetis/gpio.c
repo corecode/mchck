corecode/mchck

commit b343aaf86f8dd4f53b6c5b9c4f17324275664795

gpio: fix toggle

src/soc/kinetis/gpio.c
@@ -49,7 +49,7 @@ gpio_write(enum gpio_pin_id pin, enum gpio_pin_value val)
 void
 gpio_toggle(enum gpio_pin_id pin)
 {
-        bf_set(GPIO_PTOR_REG(gpio_physgpio_from_pin(pin)), pin_physpin_from_pin(pin), 1, 1);
+        GPIO_PTOR_REG(gpio_physgpio_from_pin(pin) = 1 << pin_physpin_from_pin(pin);
 }
 
 enum gpio_pin_value

commit 8cc06dcc1c539babd1a1f3621fa472ea5e2896ff

gpio: fix syntax broken in b343aaf

src/soc/kinetis/gpio.c
@@ -49,7 +49,7 @@ gpio_write(enum gpio_pin_id pin, enum gpio_pin_value val)
 void
 gpio_toggle(enum gpio_pin_id pin)
 {
-        GPIO_PTOR_REG(gpio_physgpio_from_pin(pin) = 1 << pin_physpin_from_pin(pin);
+        GPIO_PTOR_REG(gpio_physgpio_from_pin(pin)) = 1 << pin_physpin_from_pin(pin);
 }
 
 enum gpio_pin_value